4f01bf37cc6fe67aff448ebaf96452f847dafa47
百瀬篤志 <>
2022-03-16 23:49:04 +0900

    EntityModel追加

--- UTube/UTubeEntity.swift
+++ UTube/UTubeEntity.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+struct UTubeEntity: Codable {
+    
+    var pageInfo: pInfo?
+    var etag: String?
+    var kind: String?
+    var items: [itemInfoList]?
+    var nextPageToken: String?
+    var regionCode: String?
+    
+    struct pInfo: Codable {
+        var resultsPerPage: NSInteger?
+        var totalResults: NSInteger?
+    }
+    
+    struct itemInfoList: Codable {
+        var etag: String?
+        var kind: String?
+        var id: idItems?
+        var snippet: snipetItem?
+    }
+    
+    struct idItems: Codable {
+        var kind: String?
+        var videoId: String?
+    }
+    
+    struct snipetItem: Codable {
+        var thumbnails: thumbnailList?
+        var channelId: String?
+        var title: String?
+        var publishedAt: String?
+        var description: String?
+        var liveBroadcastContent: String?
+        var channelTitle: String?
+    }
+    
+    struct thumbnailList: Codable {
+        var `default`: defaultItems?
+        var high: defaultItems?
+        var medium: defaultItems?
+    }
+    
+    struct defaultItems: Codable {
+        var url: String?
+        var width: NSInteger?
+        var height: NSInteger?
+    }
+}
+
+
